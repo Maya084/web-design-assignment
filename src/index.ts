ddc6b53e502f46373e0c700d31137042c0c839ab
Maya084 <37115588+Maya084@users.noreply.github.com>
2024-06-03 20:45:37 +0200

    init layout

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 const message: string = 'test!';
 console.log(message);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modalButton = document.querySelector('.open-modal') as HTMLButtonElement;
+    modalButton.addEventListener('click', () => {
+        alert('Modal content goes here');
+    });
+});
